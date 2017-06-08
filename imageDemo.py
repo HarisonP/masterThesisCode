d2e340bd9b1e1619f63051cc9f8206ee509539c6
Hristo Avramov <havramov@asteasolutions.com>
2017-06-08 23:44:54 +0300

    two different z-normalizations added

--- imageDemo.py
+++ imageDemo.py
@@ -18,54 +18,66 @@ ap.add_argument("-i", "--image", required=True,
     help="path to input image")
 args = vars(ap.parse_args())
 
-# feature_extractor = FaceFeatureExtractor(args["image"])
-# feature_extractor.get_face_features()
-# feature_extractor.print_face_detected_with_shape()
-# feature_extractor.print_features()
+
+def image_features_demo():
+    feature_extractor = FaceFeatureExtractor(args["image"])
+    feature_extractor.get_face_features()
+    feature_extractor.print_face_detected_with_shape()
+    feature_extractor.print_features()
 
 
 def output(models_trainer, prefix, dataset_size):
     print("Number of photos:", dataset_size)
-    # tree_scores = np.array([math.fabs(s) for s in models_trainer.cross_val_tree()])
-    # tree_reduced_scaled_scores = np.array([math.fabs(s) for s in models_trainer.cross_val_reduced_scaled_features_tree()])
-    # print(prefix, "Tree Cross Valid Error: %0.2f (+/- %0.2f)" % (tree_scores.mean(), tree_scores.std() * 2))
-    # print(prefix, "PCA reduced scaled01 Tree Corss Valid Error: %0.2f (+/- %0.2f)" % (tree_reduced_scaled_scores.mean(), tree_reduced_scaled_scores.std() * 2))
+    tree_scores = np.array([math.fabs(s) for s in models_trainer.cross_val_tree()])
+
+    tree_ada_boost = np.array([math.fabs(s) for s in models_trainer.cross_val_ada_boost_tree()])
+    tree_reduced_scaled_scores = np.array([math.fabs(s) for s in models_trainer.cross_val_reduced_scaled_features_tree()])
+    # THE BEST OF TREE:
+    print(prefix, "Tree Cross Valid Error: %0.2f (+/- %0.2f)" % (tree_scores.mean(), tree_scores.std() * 2))
+
+    print(prefix, "PCA reduced scaled01 Tree Corss Valid Error: %0.2f (+/- %0.2f)" % (tree_reduced_scaled_scores.mean(), tree_reduced_scaled_scores.std() * 2))
+    print(prefix, "Tree Ada Boost Valid Error: %0.2f (+/- %0.2f)" % (tree_ada_boost.mean(), tree_ada_boost.std() * 2))
 
     # print(tree_scores)
     # print(models_trainer.abs_error_svm())
 
     svm_scores = np.array([math.fabs(s) for s in models_trainer.cross_val_scaled01_svm()])
-    # svm_pca_reduced_scores = np.array([math.fabs(s) for s in models_trainer.cross_val_reduced_pca_features_svm()])
-    # svm_unscaled = np.array([math.fabs(s) for s in models_trainer.cross_val_unscaled_svm()])
-    # svm_reduced_scaled = np.array([math.fabs(s) for s in models_trainer.cross_val_reduced_scaled_features_svm()])
+    svm_pca_reduced_scores = np.array([math.fabs(s) for s in models_trainer.cross_val_reduced_pca_features_svm()])
+    svm_unscaled = np.array([math.fabs(s) for s in models_trainer.cross_val_unscaled_svm()])
+    svm_reduced_scaled = np.array([math.fabs(s) for s in models_trainer.cross_val_reduced_scaled_features_svm()])
 
-    # svm_reduced = np.array([math.fabs(s) for s in models_trainer.cross_val_reduced_features_svm()])
+    svm_reduced = np.array([math.fabs(s) for s in models_trainer.cross_val_reduced_features_svm()])
+    svm_scores_ada_boost = np.array([math.fabs(s) for s in models_trainer.cross_val_ada_boost_svm()])
 
     # print(svm_scores)
-
+    # THE BEST OF SVM:
     print(prefix, "scaled01 SVM Cross Valid Error: %0.2f (+/- %0.2f)" % (svm_scores.mean(), svm_scores.std() * 2))
 
-    # print(prefix, "PCA Reduced SVM Cross Valid Error: %0.2f (+/- %0.2f)" % (svm_pca_reduced_scores.mean(), svm_pca_reduced_scores.std() * 2))
-    # unscaled
-    # print(prefix, "PCA reduced scaled01 SVM Cross Valid Error: %0.2f (+/- %0.2f)" % (svm_reduced_scaled.mean(), svm_reduced_scaled.std() * 2))
-    # print(prefix, "reduced scaled01 SVM Cross Valid Error: %0.2f (+/- %0.2f)" % (svm_reduced.mean(), svm_reduced.std() * 2))
+    print(prefix, "PCA Reduced SVM Cross Valid Error: %0.2f (+/- %0.2f)" % (svm_pca_reduced_scores.mean(), svm_pca_reduced_scores.std() * 2))
+
+    print(prefix, "PCA reduced scaled01 SVM Cross Valid Error: %0.2f (+/- %0.2f)" % (svm_reduced_scaled.mean(), svm_reduced_scaled.std() * 2))
+    print(prefix, "reduced scaled01 SVM Cross Valid Error: %0.2f (+/- %0.2f)" % (svm_reduced.mean(), svm_reduced.std() * 2))
+    print(prefix, "scaled01 SVM Ada Boost Valid Error: %0.2f (+/- %0.2f)" % (svm_scores_ada_boost.mean(), svm_scores_ada_boost.std() * 2))
+
 
 
     knn_score = np.array([math.fabs(s) for s in models_trainer.cross_val_knn()])
     knn_reduced_scaled_score = np.array([math.fabs(s) for s in models_trainer.cross_val_reduced_scaled_features_knn()])
     knn_reduced = np.array([math.fabs(s) for s in models_trainer.cross_val_reduced_features_svm()])
     # print(knn_score)
-    print(prefix, "KNN Cross Valid Error: %0.2f (+/- %0.2f)" % (knn_score.mean(), knn_score.std() * 2))
+    # print(prefix, "KNN Cross Valid Error: %0.2f (+/- %0.2f)" % (knn_score.mean(), knn_score.std() * 2))
+    # THE BEST OF KNN
     print(prefix, "PCA reduced scaled01 KNN Cross Valid Error: %0.2f (+/- %0.2f)" % (knn_reduced_scaled_score.mean(), knn_reduced_scaled_score.std() * 2))
     print(prefix, "reduced scaled01 KNN Cross Valid Error: %0.2f (+/- %0.2f)" % (knn_reduced.mean(), knn_reduced.std() * 2))
 
-    # knn_score_radius = np.array([math.fabs(s) for s in models_trainer.cross_val_knn_radius()])
-    # print(knn_score_radius)
-    # print(prefix, "KNN radius Cross Valid Error: %0.2f (+/- %0.2f)" % (knn_score_radius.mean(), knn_score_radius.std() * 2))
-
+# image_features_demo()
 
 scores_extractor = ScoresExtractor( glob.glob(os.path.realpath('./scores/*.txt')))
-scores = scores_extractor.extract_average_scores()
+scores_avr = scores_extractor.extract_average_scores()
+scores_scaled_average = scores_extractor.extract_z_scaled_average()
+scores_scaled = scores_extractor.extract_z_scaled()
+# print(scores_avr, scores_scaled)
+
 all_images = glob.glob(os.path.realpath('./dataset/*.jpg'))
 all_women = glob.glob(os.path.realpath('./dataset/women/*.jpg'))
 all_men = glob.glob(os.path.realpath('./dataset/men/*.jpg'))
@@ -74,53 +86,60 @@ features = {}
 features_men = {}
 features_women = {}
 
-with open('features_men.json') as json_data:
-    features_men = json.load(json_data, object_pairs_hook=OrderedDict)
+
+def load_features_from_file(features_filename):
+    with open(features_filename) as json_data:
+        return json.load(json_data, object_pairs_hook=OrderedDict)
+
+def extract_and_load_to_file(features, features_men, features_women):
+    for filename in all_images:
+        next_feature_extractor = FaceFeatureExtractor(filename)
+        features[os.path.basename(filename)] = next_feature_extractor.get_face_features()
+
+    for filename in all_women:
+        next_feature_extractor = FaceFeatureExtractor(filename)
+        features_women[os.path.basename(filename)] = next_feature_extractor.get_face_features()
 
 
-with open('features_women.json') as json_data:
-    features_women = json.load(json_data, object_pairs_hook=OrderedDict)
-
-with open('features.json') as json_data:
-    features = json.load(json_data, object_pairs_hook=OrderedDict)
-
-# for filename in all_images:
-#     print(filename)
-#     next_feature_extractor = FaceFeatureExtractor(filename)
-#     features[os.path.basename(filename)] = next_feature_extractor.get_face_features()
+    for filename in all_men:
+        next_feature_extractor = FaceFeatureExtractor(filename)
+        features_men[os.path.basename(filename)] = next_feature_extractor.get_face_features()
 
 
-# for filename in all_women:
-#     next_feature_extractor = FaceFeatureExtractor(filename)
-#     features_women[os.path.basename(filename)] = next_feature_extractor.get_face_features()
+    with open('features_women.json', 'w') as outfile:
+        json.dump(features_women, outfile)
+
+    with open('features_men.json', 'w') as outfile:
+        json.dump(features_men, outfile)
 
 
-# for filename in all_men:
-#     next_feature_extractor = FaceFeatureExtractor(filename)
-#     features_men[os.path.basename(filename)] = next_feature_extractor.get_face_features()
+    with open('features.json', 'w') as outfile:
+        json.dump(features, outfile)
 
+features = load_features_from_file('features.json')
+features_men = load_features_from_file('features_men.json')
+features_women = load_features_from_file('features_women.json')
 
-# with open('features_women.json', 'w') as outfile:
-#     json.dump(features_women, outfile)
+# extract_and_load_to_file(features, features_men, features_women)
 
-# with open('features_men.json', 'w') as outfile:
-#     json.dump(features_men, outfile)
+def train_3_models(feature, scores, prefix):
+    print(prefix)
+    print("Start ======================= Start")
+    models_trainer_mixed = ModelsTrainer(features, scores)
+    models_trainer_women = ModelsTrainer(features_women, scores)
+    models_trainer_men = ModelsTrainer(features_men, scores)
 
+    output(models_trainer_mixed, "Mixed", len(features));
+    output(models_trainer_women, "Women", len(features_women));
+    output(models_trainer_men, "Men",  len(features_men));
 
-# with open('features.json', 'w') as outfile:
-#     json.dump(features, outfile)
+    print("End ======================= End")
 
-models_trainer_mixed = ModelsTrainer(features, scores)
-models_trainer_women = ModelsTrainer(features_women, scores)
+# train_3_models(features, scores_avr, "Scores Average")
+# train_3_models(features, scores_scaled_average, "Scores Z-scaled Average")
+train_3_models(features, scores_scaled, "Scores Z-scaled")
 
-models_trainer_men = ModelsTrainer(features_men, scores)
-
-output(models_trainer_mixed, "Mixed", len(features));
-output(models_trainer_women, "Women", len(features_women));
-output(models_trainer_men, "Men",  len(features_men));
-
-
-
+# TODO define method for this action.
 # reg_tree = models_trainer_women.train_full_tree()
 # reg_svm = models_trainer_women.train_full_svm()
 # feature_extractor = FaceFeatureExtractor(args["image"])
--- scores_extractor.py
+++ scores_extractor.py
@@ -1,5 +1,7 @@
 import json
 from collections import defaultdict
+import numpy as np
+from sklearn import preprocessing
 
 class ScoresExtractor:
     def __init__(self, file_names):
@@ -9,7 +11,6 @@ class ScoresExtractor:
     def extract_average_scores(self):
         sum_of_scores = defaultdict(lambda: 0, {})
         average_scores = {}
-        print(len(self.filenames))
         for filename in self.filenames:
             with open(filename) as json_file:
                 next_scores = json.load(json_file)
@@ -19,3 +20,56 @@ class ScoresExtractor:
         for key, value in sum_of_scores.items():
             average_scores[key] = value / self.number_of_scores
         return average_scores
+
+    def get_z_normlized_sum_of_scores(self):
+        sum_of_scores = defaultdict(lambda: 0, {})
+
+
+        for filename in self.filenames:
+            with open(filename) as json_file:
+                next_scores = json.load(json_file)
+                next_scores_row_labels = np.array([item[0] for item in next_scores.items()])
+                next_scores_row = np.array([item[1] for item in next_scores.items()])
+                avr = next_scores_row.mean()
+                std = next_scores_row.std()
+
+                new_scores = [((score - avr) / std) for score in next_scores_row]
+
+                for index, key in enumerate(next_scores_row_labels):
+                    sum_of_scores[key] += new_scores[index]
+
+        return sum_of_scores
+
+    def extract_z_scaled_average(self):
+        #
+        sum_of_scores = self.get_z_normlized_sum_of_scores()
+        average_scores = {}
+
+        for key, value in sum_of_scores.items():
+            average_scores[key] = value / self.number_of_scores
+
+
+        average_scores_row_labels = np.array([item[0] for item in average_scores.items()])
+        average_scores_row = np.array([item[1] for item in average_scores.items()])
+
+        min_max_scaler = preprocessing.MinMaxScaler(feature_range = (1, 10))
+        average_scores_row_scaled = min_max_scaler.fit_transform(average_scores_row.transpose())
+
+        for index, key in enumerate(average_scores_row_labels):
+            average_scores[key] = average_scores_row_scaled[index]
+
+        return average_scores
+
+    def extract_z_scaled(self):
+        sum_of_scores = self.get_z_normlized_sum_of_scores()
+
+        sum_of_scores_row_labels = np.array([item[0] for item in sum_of_scores.items()])
+        sum_of_scores_row = np.array([item[1] for item in sum_of_scores.items()])
+
+        min_max_scaler = preprocessing.MinMaxScaler(feature_range = (1, 10))
+        sum_of_scores_row_scaled = min_max_scaler.fit_transform(sum_of_scores_row.transpose())
+
+        for index, key in enumerate(sum_of_scores_row_labels):
+            sum_of_scores[key] = sum_of_scores_row_scaled[index]
+
+        return sum_of_scores
